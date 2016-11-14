7cbc5ec5faa89892e165cdba740b6b8e661ffcf9
Bert Massop <b.massop@quintor.nl>
2016-11-14 18:12:10 +0100

    Use dependency injection for Configuration
    Static inclusion does not work anymore with recent versions of Play

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,10 @@ package controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import play.Configuration;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
@@ -16,24 +20,34 @@ import com.twilio.sdk.verbs.Say;
 import com.twilio.sdk.verbs.TwiMLException;
 import com.twilio.sdk.verbs.TwiMLResponse;
 
+@Singleton
 public class Application extends Controller {
 
     // Load configuration from [project]/conf/application.conf
-    public static String TWILIO_ACCOUNT_SID = play.Play.application().configuration().getString("twilio.accountSid");
-    public static String TWILIO_AUTH_TOKEN = play.Play.application().configuration().getString("twilio.authToken");
-    public static String TWILIO_NUMBER = play.Play.application().configuration().getString("twilio.number");
+    private final String twilioAccountSid;
+    private final String twilioAuthToken;
+    private final String twilioNumber;
 
     // create an authenticated REST client
-    public static TwilioRestClient client = new TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
-    public static Account mainAccount = client.getAccount();
+    private final TwilioRestClient client;
+    private final Account mainAccount;
+
+    @Inject public Application(Configuration configuration) {
+        twilioAccountSid = configuration.getString("twilio.accountSid");
+        twilioAuthToken = configuration.getString("twilio.authToken");
+        twilioNumber = configuration.getString("twilio.number");
+
+        client = new TwilioRestClient(twilioAccountSid, twilioAuthToken);
+        mainAccount = client.getAccount();
+    }
   
     // Render the home page for our application
-    public static Result index() {
+    public Result index() {
         return ok(index.render());
     }
 
     // Handle a POST request to make an outbound call
-    public static Result call() throws TwilioRestException {
+    public Result call() throws TwilioRestException {
         // Get POST data
         Map<String, String[]> params = request().body().asFormUrlEncoded();
         String to = params.get("to")[0];
@@ -42,7 +56,7 @@ public class Application extends Controller {
         CallFactory callFactory = mainAccount.getCallFactory();
         Map<String, String> callParams = new HashMap<String, String>();
         callParams.put("To", to);
-        callParams.put("From", TWILIO_NUMBER);
+        callParams.put("From", twilioNumber);
         callParams.put("Url", "http://twilio-elearning.herokuapp.com/starter/voice.php");
         callFactory.create(callParams);
         
@@ -50,7 +64,7 @@ public class Application extends Controller {
     }
 
     // Handle a POST request to send a text message
-    public static Result message() throws TwilioRestException {
+    public Result message() throws TwilioRestException {
     	// Get POST data
         Map<String, String[]> params = request().body().asFormUrlEncoded();
         String to = params.get("to")[0];
@@ -58,7 +72,7 @@ public class Application extends Controller {
     	SmsFactory smsFactory = mainAccount.getSmsFactory();
         Map<String, String> smsParams = new HashMap<String, String>();
         smsParams.put("To", to);
-        smsParams.put("From", TWILIO_NUMBER);
+        smsParams.put("From", twilioNumber);
         smsParams.put("Body", "Have fun with your Twilio development!");
         smsFactory.create(smsParams);
     	
@@ -66,7 +80,7 @@ public class Application extends Controller {
     }
 
     // Render a TwiML document to give instructions for an outbound call
-    public static Result hello() throws TwiMLException {
+    public Result hello() throws TwiMLException {
         TwiMLResponse response = new TwiMLResponse();
         Say one = new Say("Hello there! You have successfully configured a web hook.");
         Say two = new Say("Have fun with your Twilio development!");
